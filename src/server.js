7bc01cc6dbf421daa6a942bdd614f9b71b9d7e41
Daniel Ramos <danielfcramos7@gmail.com>
2022-02-07 22:50:58 -0500

    create a gihub remote repository

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 const app = require("./app.js");
 
 var PORT = process.env.PORT || 8000;
+var MODE = process.env.NODE_ENV;
 
 const serverLog = () => {
   console.table({
     port: PORT,
     domine: "localhost:",
     status: "Server opened",
+    mode: MODE,
   });
 };
 
